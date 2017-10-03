5a7625c9f83c6d2064800e15a240a828ed65686c
eze <ezequiel.borenstein@gmail.com>
2017-10-03 04:37:18 +0000

    Agrego ejercicio 1

---
+++ ejercicio1.pl
@@ -0,0 +1,17 @@
+#!/bin/perl -w
+
+use Bio::Seq;
+use Bio::SeqIO;
+
+$seq_input_obj = Bio::SeqIO->new(-file => "mRNAEjercicio1.gb", 
+                             -format => "GenBank",
+			     -desc => "Ejercicio 1",
+			     -alphabet => "rna");
+$seq_rna_obj = $seq_input_obj->next_seq;
+$prot_obj = $seq_rna_obj->translate(-frame => 1); # Uso el frame 1 porque la numeración arranca en 0, y el ORF es el 2 (arrancando desde el 1)
+$seq_output_obj = Bio::SeqIO->new(-file =>'>aminoacidosEjercicio1.fasta', 
+                             -format => 'fasta' );
+$seq_output_obj->write_seq($prot_obj);
+
+
+
