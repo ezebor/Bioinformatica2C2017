acae3a634dd2f1b3a99df0cbe1dcec59472737aa
eze <ezequiel.borenstein@gmail.com>
2017-10-13 20:52:44 +0000

    Agrego MSA al gitignore

--- Ex1/Ex1.pl
+++ Ex1/Ex1.pl
@@ -8,7 +8,7 @@ $seq_input_obj = Bio::SeqIO->new(-file => "mRNAEjercicio1.gb",
 			     -desc => "Ejercicio 1",
 			     -alphabet => "rna");
 $seq_rna_obj = $seq_input_obj->next_seq;
-$prot_obj = $seq_rna_obj->translate(-frame => 1); # Uso el frame 1 porque la numeración arranca en 0, y el ORF es el 2 (arrancando desde el 1)
+$prot_obj = $seq_rna_obj->translate(-orf => 'longest', -start => 'atg'); # Uso el frame 1 porque la numeración arranca en 0, y el ORF es el 2 (arrancando desde el 1)
 $seq_output_obj = Bio::SeqIO->new(-file =>'>aminoacidosEjercicio1.fasta', 
                              -format => 'fasta' );
 $seq_output_obj->write_seq($prot_obj);
