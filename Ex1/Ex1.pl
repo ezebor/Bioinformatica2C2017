dfde009abc988de0d8e97d6b704a72f08707447c
eze <ezequiel.borenstein@gmail.com>
2017-10-14 00:26:55 +0000

    Agrego parámetro al translate para que no imprima el * final

--- Ex1/Ex1.pl
+++ Ex1/Ex1.pl
@@ -8,7 +8,7 @@ $seq_input_obj = Bio::SeqIO->new(-file => "mRNAEjercicio1.gb",
 			     -desc => "Ejercicio 1",
 			     -alphabet => "rna");
 $seq_rna_obj = $seq_input_obj->next_seq;
-$prot_obj = $seq_rna_obj->translate(-orf => 'longest', -start => 'atg');
+$prot_obj = $seq_rna_obj->translate(-orf => 'longest', -start => 'atg', -complete => 1);
 $seq_output_obj = Bio::SeqIO->new(-file =>'>aminoacidosEjercicio1.fasta', 
                              -format => 'fasta' );
 $seq_output_obj->write_seq($prot_obj);
